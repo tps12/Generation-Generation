e8e9a9e0d9ab7650c92725cb082df080753a1afa
tps12 <tps12@columbia.edu>
2011-02-08 11:19:13 -0500

    Click for family number.

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ display.set_caption('Generations')
 
 def noiseat(x, y):
     return 1 + noise.snoise2(
-            x,
-            y / 1000.0,
-            12,
+            x/10.0,
+            y/800.0,
+            24,
             1)
+
+def sex(x, y):
+    return noise.snoise2((x+6253),(y-1220),1,1) > 0
            
 background = Surface(screen.get_size())
 background.lock()
@@ -32,14 +35,19 @@ screen.blit(background, (0,0))
 
 sprites = Group()
 
-for y in range(0, background.get_height(), 8):
-    for x in range(0, background.get_width(), 8):
-        if noiseat(x,y) > 0.9:
+def personat(x,y):
+    return noiseat(x*8,y*8) <= 0.95
+
+for x in range(0, background.get_width(), 8):
+    for y in range(0, background.get_height(), 8):
+        if not personat(x/8, y/8):
             continue
         
         sprite = Sprite()
         sprite.image = Surface((4,4), flags=SRCALPHA)
-        draw.circle(sprite.image, (255,255,0), (2,2), 2, 1)
+        male = sex(x,y)
+        draw.circle(sprite.image, (196,196,255) if male else (255,196,196),
+                    (2,2), 2, 1)
         sprite.rect = sprite.image.get_rect().move(x, y)
         sprites.add(sprite)
 
@@ -54,6 +62,26 @@ while not done:
         elif e.type == KEYDOWN:
             if e.key == K_ESCAPE:
                 done = True
+        elif e.type == MOUSEBUTTONDOWN and e.button == 1:
+            for sprite in sprites:
+                if sprite.rect.collidepoint(e.pos):
+                    generation = sprite.rect.left / 8
+                    index = sprite.rect.top / 8
+
+                    if generation > 0:
+                        family = 0
+                        infam = False
+                        for i in range(0, index):
+                            if personat(generation, i):
+                                if not infam:
+                                    infam = True
+                            else:
+                                if infam:
+                                    infam = False
+                                    family += 1
+                        print family
+                    
+                    break
 
     sprites.update()
     sprites.clear(screen, background)
